3b66c827ad91cf3d8f00f2929b96d9693143137c
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-08-28 14:51:41 -0300

    chore: add reflect-metadata

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,24 @@
-import app from './app';
+import 'reflect-metadata';
+
+import express from 'express';
+import routes from './routes';
+import './database';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333');
+  console.log('Server Started - http://localhost:3333');
 });
