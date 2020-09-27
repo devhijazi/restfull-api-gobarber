6a61a771298d1f9fed9c68345043b274d3b9fa53
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-09-26 21:59:02 -0300

    chore(DDD): changed archteture of backend to DDD

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,13 +2,13 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
-const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tempFolder,
+  directory: tmpFolder,
 
   storage: multer.diskStorage({
-    destination: tempFolder,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('HEX');
       const fileName = `${fileHash}-${file.originalname}`;
